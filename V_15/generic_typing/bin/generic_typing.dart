28d479c7323ab086128f2196e86a3af98f3c5820
Nam H Bui <77969945+NamHaiBui@users.noreply.github.com>
2023-03-22 21:57:36 -0400

    start on Generic

---
+++ V_15/generic_typing/bin/generic_typing.dart
@@ -0,0 +1,5 @@
+import 'package:generic_typing/generic_typing.dart' as generic_typing;
+
+void main(List<String> arguments) {
+  print('Hello world: ${generic_typing.calculate()}!');
+}
---
+++ V_15/generic_typing/test/generic_typing_test.dart
@@ -0,0 +1,8 @@
+import 'package:generic_typing/generic_typing.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
